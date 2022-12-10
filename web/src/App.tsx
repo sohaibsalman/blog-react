09eee7b5dcf1289ae525b7e3553cd8d925e10547
sohaibsalman <sohaibsalman10@gmail.com>
2022-12-11 02:00:22 +0500

    Add hook to update site title

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,11 @@
 import { Navbar } from "./components/Navbar";
+import { usePageTitle } from "./hooks/usePageTitle";
+
+import { config as globalConfig } from "./config/global.config";
 
 const App: React.FC = () => {
+  usePageTitle(globalConfig.siteTitle);
+
   return <Navbar />;
 };
 
---
+++ web/src/config/global.config.tsx
@@ -0,0 +1,5 @@
+const config = {
+  siteTitle: "Sohaib Salman",
+};
+
+export { config };
